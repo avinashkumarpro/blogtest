a5267a9ac547257076b81e31bc4538e619b7af54
Git User <gituser@example.com>
2017-05-02 13:34:44 +0530

    User registration features added.

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ # it will redirect a successful login to the top-level index
+
+LOGIN_REDIRECT_URL = '/'
